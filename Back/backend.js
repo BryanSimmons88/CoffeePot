b49807a5f8f41beba7c4c0e3f1addd9fe53b4510
bryan simmons <bryan.simmons@kaseya.com>
2023-06-08 16:33:31 -0400

    Added support for older browsers Increased button size Increase font size Appended zero to minutes less than 10

--- Back/backend.js
+++ Back/backend.js
@@ -11,12 +11,12 @@ const wss = new WebSocketServer({ server });
 var lastBrewed = new Date();
 
 wss.on("connection", async (client) => {
-  client.send((lastBrewed.getHours() - 4) + ":" + lastBrewed.getMinutes());
+  client.send((lastBrewed.getHours() - 4) + ":" + (lastBrewed.getMinutes() > 9 ? lastBrewed.getMinutes() : '0' + lastBrewed.getMinutes()));
   client.addEventListener("message", (message) => {
     if (message.data === "brew") {
       lastBrewed = new Date();
       wss.clients.forEach((client) => {
-        client.send((lastBrewed.getHours() - 4) + ":" + lastBrewed.getMinutes())
+        client.send((lastBrewed.getHours() - 4) + ":" + (lastBrewed.getMinutes() > 9 ? lastBrewed.getMinutes() : '0' + lastBrewed.getMinutes()))
       })
     }
   });
