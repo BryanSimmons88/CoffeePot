a11c15b245e6e95189a0583e1dd49ea64de0db0f
bryan simmons <bryan.simmons@kaseya.com>
2023-07-12 16:33:46 -0400

    Added date to timestamp

--- Back/backend.js
+++ Back/backend.js
@@ -10,13 +10,14 @@ const wss = new WebSocketServer({ server });
 
 var lastBrewed = new Date();
 
+console.log((lastBrewed.getUTCMonth() + 1) + '/' + (lastBrewed.getUTCDate()) + ' ' + (lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()))
 wss.on("connection", async (client) => {
-  client.send((lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()));
+  client.send((lastBrewed.getUTCMonth() + 1) + '/' + (lastBrewed.getUTCDate()) + ' ' + (lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()));
   client.addEventListener("message", (message) => {
     if (message.data === "brew") {
       lastBrewed = new Date();
       wss.clients.forEach((client) => {
-        client.send((lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()))
+        client.send((lastBrewed.getUTCMonth() + 1) + '/' + (lastBrewed.getUTCDate()) + ' ' + (lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()))
       })
     }
   });
