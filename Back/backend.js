0b36bfe67771f5afc74c9d82afeb069dad0772cf
bryan simmons <bryan.simmons@kaseya.com>
2023-09-22 09:26:55 -0400

    Added moment js for datetime

--- Back/backend.js
+++ Back/backend.js
@@ -1,5 +1,6 @@
 var WebSocketServer = require("ws").WebSocketServer;
 var http = require("http");
+var moment = require('moment') 
 const PORT = 8080;
 
 var server = http
@@ -10,14 +11,13 @@ const wss = new WebSocketServer({ server });
 
 var lastBrewed = new Date();
 
-console.log((lastBrewed.getUTCMonth() + 1) + '/' + (lastBrewed.getUTCDate()) + ' ' + (lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()))
 wss.on("connection", async (client) => {
-  client.send((lastBrewed.getUTCMonth() + 1) + '/' + (lastBrewed.getUTCDate()) + ' ' + (lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()));
+  client.send(moment(lastBrewed).format('M/DD H:MM'));
   client.addEventListener("message", (message) => {
     if (message.data === "brew") {
       lastBrewed = new Date();
       wss.clients.forEach((client) => {
-        client.send((lastBrewed.getUTCMonth() + 1) + '/' + (lastBrewed.getUTCDate()) + ' ' + (lastBrewed.getUTCHours() - 4) + ":" + (lastBrewed.getUTCMinutes() > 9 ? lastBrewed.getUTCMinutes() : '0' + lastBrewed.getUTCMinutes()))
+        client.send(moment(lastBrewed).format('M/DD H:MM'))
       })
     }
   });
